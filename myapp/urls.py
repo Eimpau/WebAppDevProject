5d9225f57a083821e1968c8f2114757264f9315d
Paul <paul.mulpeter2@mail.dcu.ie>
2025-04-03 14:51:46 +0100

    changed the nav bar links to suit django

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,11 @@ from . import views
 app_name = "myapp"
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("guide/", views.guide, name="guide"),
+    path("login/", views.login, name="login"),
+    path("machineDescription/", views.machineDescription, name="machineDescription"),
+    path("products/", views.products, name="products"),
+
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -10,4 +10,26 @@ def home(request):
     context = {}
     return render(request, "myapp/Home.html", context)
 
+def about(request):
+    context = {}
+    return render(request, "myapp/AboutUs.html", context)
+
+def guide(request):
+    context = {}
+    return render(request, "myapp/Guide.html", context)
+
+def login(request):
+    context = {}
+    return render(request, "myapp/Login.html", context)
+
+def machineDescription(request):
+    context = {}
+    return render(request, "myapp/MachineDescription.html", context)
+
+def products(request):
+    context = {}
+    return render(request, "myapp/Products.html", context)
+
+
+
 
