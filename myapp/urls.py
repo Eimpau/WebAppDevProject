6cc1f93d94513c1c0a58a12b7788c308e9d84b0f
Eimantas Kozeniauskas <eimantas@kozeniauskas.com>
2025-04-07 21:19:54 +0100

    changed login view name to be called employee login for django to work better

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("guide/", views.guide, name="guide"),
-    path("login/", views.login, name="login"),
+    path("employee_login/", views.employee_login, name="employee_login"),
     path("machines/", views.machines, name="machines"),
     path("products/", views.products, name="products"),
 ]
