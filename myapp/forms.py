e588881fdfb5f98f7305198fa971768a4cd2b0d8
Eimantas Kozeniauskas <eimantas@kozeniauskas.com>
2025-04-10 09:32:23 +0100

    Added user registration and account information

--- myapp/forms.py
+++ myapp/forms.py
@@ -6,3 +6,31 @@ from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150, label="Username", widget=forms.TextInput(attrs={'placeholder': 'Enter your username'}))
     password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password'}))
+
+class ManagerUserRegistrationForm(forms.Form):
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={'placeholder': 'Username'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'})
+    )
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'})
+    )
+    role = forms.ChoiceField(choices=[('Manager', 'Manager'), ('Technician', 'Technician'),('Repair', 'Repair'), ('View', 'View')]
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('Username already registered.')
+        return username
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm = cleaned_data.get('confirm_password')
+        if password and confirm and password != confirm:
+            raise forms.ValidationError('Passwords do not match')
+        return cleaned_data
