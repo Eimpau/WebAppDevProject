ffe6dc8718914147a2e02e684f28cab5db3e411a
Eimantas Kozeniauskas <eimantas@kozeniauskas.com>
2025-04-13 20:17:13 +0100

    Got HTTP POST API and REST GET API working

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -2,11 +2,21 @@ from rest_framework import serializers
 
 from .models import Machine
 
-class MachineSerializer(serializers.ModelSerializer):
+class MachineWarningSerializer(serializers.Serializer):
     """
     Serializer for the Machine model.
-    This serializer converts the Machine model to a JSON format with specific fields needed for the REST API.
+    This serializer class is used to change status state of a machine.
     """
-    class Meta:
-        model = Machine
-        fields = ['id', 'name', 'status', 'updated_at']
+    id = serializers.IntegerField()  # Assuming the machine is identified by an ID.
+    status = serializers.ChoiceField(choices=[('Warning', 'Warning'), ('Fault', 'Fault'), ('OK', 'OK')])
+
+
+class MachineStatusSerializer(serializers.ModelSerializer):
+     """
+     Serializer for the Machine model.
+     This serializer converts the Machine model to a JSON format with specific fields needed for the REST API.
+     """
+     class Meta:
+         model = Machine
+         # Only display the necessary fields in the API response.
+         fields = ['id', 'name', 'status', 'updated_at']
